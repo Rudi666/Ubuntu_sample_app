b1caf4b33fa83df807ebc392248bfad40d167cb5
Joe Karas <rudi4@tampabay.rr.com>
2012-05-01 18:44:19 -0400

    Add a StaticPages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 SampleApp::Application.routes.draw do
+  get "static_pages/home"
+  get "static_pages/help"
+
+end
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -55,4 +60,4 @@ SampleApp::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
-end
+
