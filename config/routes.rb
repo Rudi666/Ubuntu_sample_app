518698a8b04c305273387c68be4c6e20a0eaf175
Joe Karas <rudi4@tampabay.rr.com>
2012-05-21 14:56:37 -0400

    Finish layout and routes. Tests not working for Users page??

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
+  get "users/new"  
+
+  root to: 'static_pages#home'
+
+  # get "static_pages/home"
+  # get "static_pages/help"
+  # get "static_pages/about"
+  # get "static_pages/contact"
+
+
+  match '/help',    to: 'static_pages#help'
+  match '/about',   to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
+  match '/signup',  to: 'users#new'
 end
   
   # The priority is based upon order of creation:
---
+++ spec/requests/user_pages_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe "UserPages" do
+  describe "GET /user_pages" do
+    it "works! (now write some real specs)" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      # get user_pages_index_path
+      # response.status.should be(200)
+      # Tests not working for Users page??
+    end
+  end
+end
