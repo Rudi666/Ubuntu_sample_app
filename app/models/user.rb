c5bf95ae0170310bf32fad0c9633d4f6e346b34e
Joe Karas <rudi4@tampabay.rr.com>
2012-05-22 19:43:58 -0400

    Make a basic User model (including secure passwords). All tests workinggit add .

---
+++ app/models/user.rb
@@ -0,0 +1,29 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime        not null
+#  updated_at :datetime        not null
+#
+
+# If you want your annotations to be up-to-date, you’ll have to run annotate again any time the data model changes.
+# bundle exec rake db:test:prepare
+# bundle exec annotate --position before
+
+class User < ActiveRecord::Base
+  attr_accessible :name, :email, :password, :password_confirmation
+  has_secure_password
+
+  before_save { |user| user.email = email.downcase }
+
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
+end
+
