623fd19cd6051370c3999226793698651a839006
Joe Karas <rudi4@tampabay.rr.com>
2012-05-25 16:58:17 -0400

    Fixed RSPEC bugs in user_pages_spec.r Need to fix authentication_pages_spec.rb. APP working fine.

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,6 +4,15 @@ describe "UserPages" do
   
 subject { page }
 
+  # Listing 9.13
+  describe "edit" do
+    let(:user) { FactoryGirl.create(:user) }
+    before do
+      sign_in user
+      visit edit_user_path(user)
+    end
+  end
+
   describe "profile page" do
     # Code to make a user variable
     let(:user) { FactoryGirl.create(:user) }
@@ -12,6 +21,59 @@ subject { page }
     it { should have_selector('h1',    text: user.name) }
     it { should have_selector('title', text: user.name) }
   end
+
+  # Listing 9.33.
+  # 59 examples, 0 failures
+  describe "index" do
+
+    let(:user) { FactoryGirl.create(:user) }
+
+    before(:all) { 30.times { FactoryGirl.create(:user) } }
+    after(:all)  { User.delete_all }
+
+    before(:each) do
+      sign_in user
+      visit users_path
+    end
+
+    it { should have_selector('title', text: 'All users') }
+    it { should have_selector('h1',    text: 'All users') }
+
+    describe "pagination" do
+
+      it { should have_selector('div.pagination') }
+
+      it "should list each user" do
+        User.paginate(page: 1).each do |user|
+          page.should have_selector('li', text: user.name)
+        end
+      end
+    end
+ 
+    # Add Delete links 
+    # Listing 9.44
+    # 63 examples, 0 failures
+    # user spec now working!!
+    describe "delete links" do
+
+      it { should_not have_link('delete') }
+
+      describe "as an admin user" do
+        let(:admin) { FactoryGirl.create(:admin) }
+        before do
+          sign_in admin
+          visit users_path
+        end
+
+        it { should have_link('delete', href: user_path(User.first)) }
+        it "should be able to delete another user" do
+          expect { click_link('delete') }.to change(User, :count).by(-1)
+        end
+        it { should_not have_link('delete', href: user_path(admin)) }
+      end
+    end
+ # End of delete links
+ end
 
   describe "signup" do
 
